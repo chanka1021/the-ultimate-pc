7820bdf37489de871d7b02a4efa1657e1602622f
Ashraf <59064232+chanka1021@users.noreply.github.com>
2022-11-09 13:52:11 +0100

    react app setup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,53 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.route('/add').post((req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const email = req.body.email;
-    const is_admin = Boolean(req.body.is_admin) ;
-    const photo = req.body.photo;
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const is_admin = Boolean(req.body.is_admin);
+  const photo = req.body.photo;
 
-    const newUser = new User({username,password,email,is_admin,photo})
+  const newUser = new User({ username, password, email, is_admin, photo });
 
-    newUser.save()
-        .then(()=> res.json('user added !'))
-        .catch(err => res.status(400).json('error : '+ err));
+  newUser
+    .save()
+    .then(() => res.json("user added !"))
+    .catch((err) => res.status(400).json("error : " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("error : " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("user deleted"))
+    .catch((err) => res.status(400).json("error : " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  User.findById(req.params.id)
+  .then(user => {
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.is_admin = Boolean(req.body.is_admin);
+    user.photo = req.body.photo;
+
+    user.save()
+    .then(() => res.json("user updated !"))
+    .catch((err) => res.status(400).json("error : " + err));
+})
+.catch((err) => res.status(400).json("error : " + err));
 });
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-    s
-    </div>
+    
+      <h1>hello world</h1>
   );
 }
 
